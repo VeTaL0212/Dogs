6e2c3f0e19ed77e4bd9d3eef76ef3ebdf1c774b2
Vitali <vitaly.u@mail.ru>
2024-11-18 23:02:25 +0300

    Добавлен прогресс-бар

--- Dogs.py
+++ Dogs.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from tkinter import  ttk
 import requests
 from PIL import Image, ImageTk
 from  io import BytesIO
 from tkinter import messagebox as mb
+
 
 
 def get_dog_image():
@@ -10,7 +12,7 @@ def get_dog_image():
         response = requests.get("https://dog.ceo/api/breeds/image/random")
         response.raise_for_status()
         data = response.json()
-        return data("message")
+        return data["message"]
     except Exception as e:
         mb.showerror("Ошибка", f"Возникла ошибка при запросе к API {e}")
         return None
@@ -30,7 +32,12 @@ def show_image():
             label.image = img
         except Exception as e:
             mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображения: {e}")
+    progress.stop()
 
+def prog():
+    progress['value'] = 0
+    progress.start(30)
+    window.after(3000, show_image)
 
 
 
@@ -39,11 +46,14 @@ window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
 
-label = Label()
+label = ttk.Label()
 label.pack(pady=10)
 
-button = Button(text="Загрузить изображение", command=show_image)
+button = ttk.Button(text="Загрузить изображение", command=prog)
 button.pack(pady=10)
+
+progress = ttk.Progressbar(mode="determinate", length=300)
+progress.pack(pady=10)
 
 window.mainloop()
 
